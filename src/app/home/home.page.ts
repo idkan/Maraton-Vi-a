26558db6ae496507ecdcc0b0172687ee1dc0cfad
idkan <abraham.serena.lk@gmail.com>
2019-05-19 12:55:53 -0400

    Initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NavController, Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  splash = true;
+  
+  constructor(public platform: Platform, public splashscreen: SplashScreen) {
+      
+    platform.ready().then(() => {
+      
+      setTimeout(() => this.splash = false,3000);
+      splashscreen.hide();
+    });
+  }
+
+}
